6fc5b9a72c90efb08ede1df9ce07f0bfe718ff6e
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-05-20 07:11:39 +0300

    10.10

--- Task_3/Task_3/Form1.cs
+++ Task_3/Task_3/Form1.cs
@@ -12,9 +12,20 @@ namespace Task_3
 {
     public partial class Form1 : Form
     {
+        int count = 0;
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count += 1;
+            if (count / count == 1)
+            {
+                button1.BackColor = Color.Yellow;
+            }
         }
     }
 }
