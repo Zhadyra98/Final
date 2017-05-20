94118f3e931ca6a026cd1f513e29066e1259fd09
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-05-20 08:21:30 +0300

    10.20

--- Task_1/Task_1/Program.cs
+++ Task_1/Task_1/Program.cs
@@ -7,39 +7,32 @@ using System.IO;
 
 namespace Task_1
 {
-
     class Program
     {
-        public static int check(string path)
-        {
-            int count = 0;
-            StreamReader sr = new StreamReader(path);
-            string k = sr.ReadLine();
-            string[] k1 = k.Split();
-            int a = int.Parse(k1[0]);
-            int b = int.Parse(k1[1]);
-            int l = Math.Max(a, b);
-            for (int i = 1; i < l; i++)
-            {
-                if (a % i == 0 && b % i == 0)
-                {
-                    count++;
-                }
-            }
-            return count;
-            sr.Close();
-        }
         static void Main(string[] args)
         {
-            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\user\final_2017\Task1");
+    
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\user\Final\Task1");
             FileInfo[] files = dir.GetFiles();
-            
             foreach(FileInfo f in files)
             {
+                StreamReader sr = new StreamReader(f.FullName);
+                string k = sr.ReadLine();
+                string[] k1 = k.Split();
+                int count = 0;
+                int a = int.Parse(k1[0]);
+                int b = int.Parse(k1[1]);
 
-                int l= check(f.FullName);
-                Console.Write(f.Name +" ");
-                if (l == 1)
+                int l = Math.Min(a, b);
+                for (int i=1; i<l; i++)
+                {
+                    if(a%i==0 && b % i == 0)
+                    {
+                        count++;
+                    }
+                }
+               Console.Write(f.Name+" ");
+                if (count == 1)
                 {
                     Console.WriteLine("yes");
 
@@ -47,9 +40,8 @@ namespace Task_1
                 else
                 {
                     Console.WriteLine("no");
-                }
-                
-                
+                }              
+                sr.Close();
             }
             
             Console.ReadKey();
