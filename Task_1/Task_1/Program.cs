20853ac5830bbec822d1e522e63d0d941462c3cf
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-05-20 06:39:04 +0300

    20

--- Task_1/Task_1/Program.cs
+++ Task_1/Task_1/Program.cs
@@ -11,12 +11,41 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+    
             DirectoryInfo dir = new DirectoryInfo(@"C:\Users\user\final_2017\Task1");
             FileInfo[] files = dir.GetFiles();
             foreach(FileInfo f in files)
             {
-                Console.WriteLine(f.Name);
+                StreamReader sr = new StreamReader(f.FullName);
+                string k = sr.ReadLine();
+                string[] k1 = k.Split();
+                int count = 0;
+                int a = int.Parse(k1[0]);
+                int b = int.Parse(k1[1]);
+
+                int l = Math.Max(a, b);
+                for (int i=1; i<l; i++)
+                {
+                    if(a%i==0 && b % i == 0)
+                    {
+                        count++;
+                    }
+                }
+
+               Console.WriteLine(f.Name);
+                if (count == 1)
+                {
+                    Console.Write("yes");
+
+                }
+                else
+                {
+                    Console.Write("no");
+                }
+                
+                sr.Close();
             }
+            
             Console.ReadKey();
         }
     }
