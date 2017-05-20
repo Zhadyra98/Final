6ca4ee22568615ee899e149153356850e87dc211
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-05-20 07:00:25 +0300

    10.00

--- Task2/Task2/Form1.Designer.cs
+++ Task2/Task2/Form1.Designer.cs
@@ -58,6 +58,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "0";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -67,6 +68,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "0";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -76,6 +78,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "0";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -85,6 +88,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "0";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -94,6 +98,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "0";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -103,6 +108,7 @@
             this.button7.TabIndex = 6;
             this.button7.Text = "0";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -112,6 +118,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "0";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -121,6 +128,7 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "0";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // textBox1
             // 
--- Task2/Task2/Form1.cs
+++ Task2/Task2/Form1.cs
@@ -20,13 +20,146 @@ namespace Task2
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            if (int.Parse(button1.Text) % 2 == 0)
+            if (int.Parse(button1.Text) % 2 == 0 && int.Parse(button1.Text) != 0)
             {
                 int a = int.Parse(button1.Text);
                 int b = int.Parse(textBox1.Text);
                 button1.Text = (a + 1).ToString();
                 textBox1.Text=(b+1).ToString();
 
+            }
+            else
+            {
+                int a = int.Parse(button1.Text);
+                button1.Text = (a + 1).ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(button2.Text);
+            if (int.Parse(button2.Text) % 2 == 0 && a!=0)
+            {   
+                int b = int.Parse(textBox1.Text);
+                button2.Text = (a + 1).ToString();
+                textBox1.Text = (b + 1).ToString();
+
+            }
+            else
+            {
+                button2.Text = (a + 1).ToString();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(button7.Text);
+            if (a % 2 == 0 && a != 0)
+            {
+                int b = int.Parse(textBox1.Text);
+                button7.Text = (a + 1).ToString();
+                textBox1.Text = (b + 1).ToString();
+
+            }
+            else
+            {
+                button7.Text = (a + 1).ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(button5.Text);
+            if (a % 2 == 0 && a != 0)
+            {
+                int b = int.Parse(textBox1.Text);
+                button5.Text = (a + 1).ToString();
+                textBox1.Text = (b + 1).ToString();
+
+            }
+            else
+            {
+                button5.Text = (a + 1).ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(button4.Text);
+            if (a % 2 == 0 && a != 0)
+            {
+                int b = int.Parse(textBox1.Text);
+                button4.Text = (a + 1).ToString();
+                textBox1.Text = (b + 1).ToString();
+
+            }
+            else
+            {
+                button4.Text = (a + 1).ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(button3.Text);
+            if (a % 2 == 0 && a != 0)
+            {
+                int b = int.Parse(textBox1.Text);
+                button3.Text = (a + 1).ToString();
+                textBox1.Text = (b + 1).ToString();
+
+            }
+            else
+            {
+                button3.Text = (a + 1).ToString();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(button6.Text);
+            if (a % 2 == 0 && a != 0)
+            {
+                int b = int.Parse(textBox1.Text);
+                button6.Text = (a + 1).ToString();
+                textBox1.Text = (b + 1).ToString();
+
+            }
+            else
+            {
+                button6.Text = (a + 1).ToString();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(button8.Text);
+            if (a % 2 == 0 && a != 0)
+            {
+                int b = int.Parse(textBox1.Text);
+                button8.Text = (a + 1).ToString();
+                textBox1.Text = (b + 1).ToString();
+
+            }
+            else
+            {
+                button8.Text = (a + 1).ToString();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(button9.Text);
+            if (a % 2 == 0 && a != 0)
+            {
+                int b = int.Parse(textBox1.Text);
+                button9.Text = (a + 1).ToString();
+                textBox1.Text = (b + 1).ToString();
+
+            }
+            else
+            {
+                button9.Text = (a + 1).ToString();
             }
         }
     }
