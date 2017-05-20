3dced1a2ba51db3500a8aaeb2ee473f7e0000f94
Zhadyra <oralkhanova.zhadyra@gmail.com>
2017-05-20 06:16:43 +0300

    First task

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\user\final_2017\Task1");
+            FileInfo[] files = dir.GetFiles();
+            foreach(FileInfo f in files)
+            {
+                Console.WriteLine(f.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
